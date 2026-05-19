83838551977f7cdbf591065fbc4aa3dfb3fc22d6
Víctor <vroigtenes@gmail.com>
2026-05-19 23:18:34 +0200

    Add first JSF managed bean and PrimeFaces page

---
+++ src/com/soc/ewok/bean/HomeBean.java
@@ -0,0 +1,12 @@
+package com.soc.ewok.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HomeBean {
+
+    public String getMessage() {
+        return "PrimeFaces + JSF working correctly";
+    }
+
+}
