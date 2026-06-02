7feabadcfe84c734644496ceaee36b0e36032ba9
Víctor <vroigtenes@gmail.com>
2026-06-02 16:15:14 +0200

    Primer ProducteBean

---
+++ src/com/soc/ewok/bean/MostraProdCategBean.java
@@ -0,0 +1,69 @@
+package com.soc.ewok.bean;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import com.soc.ewok.controller.EWokController;
+import com.soc.ewok.dao.ProducteDAO;
+import com.soc.ewok.model.Producte;
+
+@ManagedBean
+@ViewScoped
+public class MostraProdCategBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String codi;
+    private List<Producte> productesFiltrats;
+    private boolean errorNoProd;
+    private boolean errorParamCodi;
+
+    @PostConstruct
+    public void init() {
+        Map<String, String> params = FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .getRequestParameterMap();
+
+        codi = params.get("codi");
+        if (codi == null || codi.trim().isEmpty()) {
+            errorParamCodi = true;
+            productesFiltrats = Collections.emptyList();
+            errorNoProd = true;
+            return;
+        }
+
+        try {
+            ProducteDAO dao = new ProducteDAO(EWokController.getGlobalDatasource());
+            productesFiltrats = dao.obtenirTotsXTipusIdVigents(codi);
+            errorNoProd = productesFiltrats == null || productesFiltrats.isEmpty();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            productesFiltrats = Collections.emptyList();
+            errorNoProd = true;
+        }
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public List<Producte> getProductesFiltrats() {
+        return productesFiltrats;
+    }
+
+    public boolean isErrorNoProd() {
+        return errorNoProd;
+    }
+
+    public boolean isErrorParamCodi() {
+        return errorParamCodi;
+    }
+}
